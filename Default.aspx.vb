446855d7e8cef721f9571790c8fd41c03f39dc66
Greg Mazunik <gmazunik@gmail.com>
2013-10-09 23:06:09 -0500

    Improved logic, validation, and style

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -10,43 +10,39 @@ Partial Class _Default
         Dim preTaxD As Double
         Dim afterTaxD As Double
         Dim grossPay As Double
+        Dim taxablePay As Double
+        Dim tax As Double
+        Dim postTaxPay As Double
         Dim netPay As Double
 
         ' Convert user input to appropriate variable type
         hrWage = CDbl(tbHourlyWage.Text)
         hrWorked = CDbl(tbHoursWorked.Text)
+        preTaxD = CDbl(tbPreTax.Text)
+        afterTaxD = CDbl(tbAfterTax.Text)
 
         ' Calculate gross pay
         grossPay = (hrWage * hrWorked)
 
-        ' Factor in deductions and taxes, display error handling
-        If tbPreTax.Text <> String.Empty Then
-            Try
-                preTaxD = CDbl(tbPreTax.Text)
-                grossPay -= preTaxD
-            Catch formatExceptionParameter As InvalidCastException
-                MsgBox("If you have a pre-tax deduction, please enter a positive number in the appropriate box.")
-            End Try
+        ' Factor in deductions and taxes
+        taxablePay = grossPay - preTaxD
+            
+        ' Calculate tax rate
+        If grossPay < 500 Then
+            tax = taxablePay * 0.18
+        Else
+            tax = taxablePay * 0.22
         End If
 
-        Select Case grossPay
-            Case Is < 500
-                netPay = grossPay - (grossPay * 0.18)
-            Case Else
-                netPay = grossPay - (grossPay * 0.22)
-        End Select
+        ' Calculate post-tax pay amount
+        postTaxPay = taxablePay - tax
 
-        If tbAfterTax.Text <> String.Empty Then
-            Try
-                afterTaxD = CDbl(tbAfterTax.Text)
-                netPay -= afterTaxD
-            Catch formatExceptionParameter As InvalidCastException
-                MsgBox("If you have a pre-tax deduction, please enter a positive number in the appropriate box.")
-            End Try
-        End If
+        ' Calculate net pay, subtracting any after-tax deductions
+        netPay = postTaxPay - afterTaxD
 
         'Display results in label
         lblDisplayNetPay.Text = String.Format("{0:C}", netPay)
+
     End Sub
 
     Protected Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
@@ -56,6 +52,9 @@ Partial Class _Default
         tbHoursWorked.Text = ""
         tbPreTax.Text = ""
         tbAfterTax.Text = ""
+
+        ' Focus on wage textbox
+        tbHourlyWage.Focus()
 
     End Sub
 End Class
